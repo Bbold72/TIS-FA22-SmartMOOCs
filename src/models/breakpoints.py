f11beac5b37a5d76bf3aa5d4c7a38a77109fc38d
Bbold72 <reinbold.brian.j@gmail.com>
2022-11-24 19:48:10 -0500

    calculate breakpoints

--- src/models/breakpoints.py
+++ src/models/breakpoints.py
@@ -26,14 +26,31 @@ DATA_RAW_DIR = Path.joinpath(DATA_DIR, 'raw/cs-410')
 INTERMEDATE_DATA_DIR = Path.joinpath(DATA_DIR, 'intermediate')
 
 
-def calc_breakpoints(corpus: Corpus) -> List[int]:
-    algo = rpt.Pelt(model="rbf").fit(corpus.ts_cos_similarity)
-    std = np.std(corpus.ts_cos_similarity)
+def calc_breakpoints(time_series: np.array) -> List[int]:
+    '''
+    calcualates the breakpoints of a time series using Pelt
+
+    Args:
+        - time_series: np.array - time series of correlations
+    
+    returns: List[int] - indexes of time series breakpoitns
+    '''
+    algo = rpt.Pelt(model="rbf").fit(time_series)
+    std = np.std(time_series)
     result = algo.predict(pen=std)
 
     return result
 
 def merge_documents_breakpoints(documents: List[Segment], breakpoints: int) -> List[Segment]:
+    '''
+    combine list of documents based on breakpoints
+
+    Args:
+        - documents: List[Segment] - list of transcript segments
+        - breakpoints: List[int] - indexes of time series breakpoitns
+    
+    returns: List[Segment] - list of transcript segments
+    '''
     subtopic_docs = []
     start_idx = 0
     for id, end_idx in enumerate(breakpoints):
@@ -52,32 +69,41 @@ if __name__ == '__main__':
     # file_name = '04_week-4/02_week-4-lessons/01_lesson-4-1-probabilistic-retrieval-model-basic-idea'
     # corpus_times = corpuses[file_name]
     dataframes = []
-    topic_transitions_corpuses = dict()        
-    for transcript_name, corpus_times in corpuses.items():
+    topic_transitions_corpuses = dict()       
+
+    for transcript_name, corpus_times in corpuses.items():    # loop through each transcript
         topic_transitions_corpuses[transcript_name] = dict()
 
-        for time_delta, corpus in corpus_times.items():
-            breakpoints = calc_breakpoints(corpus)
-            print(breakpoints)
-    
+        for time_delta, corpus in corpus_times.items():       # loop through each time interval
 
+            # calc breakpoint and make new document corpus based on them
+            breakpoints = calc_breakpoints(corpus.ts_cos_similarity)    
             subtopic_docs = merge_documents_breakpoints(corpus.documents, breakpoints)
             topic_transitions_corpus = Corpus(corpus.vocab, subtopic_docs)
             topic_transitions_corpus.create_term_doc_freq_matrix()
             topic_transitions_corpus.calc_similarity_ts()
 
-            topic_transitions_corpuses[transcript_name][time_delta] = corpus
+            # add to dict
+            topic_transitions_corpuses[transcript_name][time_delta] = topic_transitions_corpus
 
-
+            # creae dataframe of results
             df = pd.DataFrame(subtopic_docs)
             df['file_name'] = transcript_name
             df['time_interval'] = time_delta
             dataframes.append(df)
 
-    output_file = lambda ext: Path.joinpath(INTERMEDATE_DATA_DIR, f'transcripts.{ext}')
+    # output file to intermediate folder taking file extension as input
+    output_file = lambda ext: Path.joinpath(INTERMEDATE_DATA_DIR, f'topic_transitions.{ext}')
 
+    # output data as pickle file
+    print('saving corpuses')
+    with open(output_file('pkl'), 'wb') as f:
+        pickle.dump(topic_transitions_corpuses, f)
+
+
+    # export dataframe
     df = pd.concat(dataframes, axis=0)
-    df.to_csv(Path.joinpath(INTERMEDATE_DATA_DIR, 'topic_transitions.csv'))
+    df.to_csv(output_file('csv'), index=False)
     print(df)
     print(df.groupby(['file_name', 'time_interval']).agg({'end' : ['count', 'max']}))
             
